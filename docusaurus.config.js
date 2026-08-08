c07d57190e2718b151bb7476ef89eb7e85c76203
abraaosantosdev <abraaofilipi12@gmail.com>
2026-08-08 16:09:42 -0300

    Url de documentações atualizado

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -23,12 +23,12 @@ const config = {
   url: 'https://abraaosantosdeveloper.github.io',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/blabry/',
+  baseUrl: '/docs/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
   organizationName: 'abraaosantosdeveloper', // Usually your GitHub org/user name.
-  projectName: 'blabry', // Usually your repo name.
+  projectName: 'blabry-docs', // Usually your repo name.
 
   onBrokenLinks: 'throw',
 
