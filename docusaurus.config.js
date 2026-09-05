ba34a2227f9c2447a4b7723d6d61ca2edadf2553
abraaosantosdev <abraaofilipi12@gmail.com>
2026-09-05 01:24:42 -0300

    updated baseurl

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -20,10 +20,10 @@ const config = {
   },
 
   // Set the production url of your site here
-  url: 'https://abraaosantosdeveloper.github.io',
+  url: 'https://docs.blabry.com.br',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/blabry-docs/',
+  baseUrl: '/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
